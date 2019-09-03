62a0aa40d9977d0d505e3585779d8139e1ae8af0
Matteo Riva <matteo.riva@soisy.it>
2019-09-03 15:04:49 +0200

    Fix config.php array syntax

--- config/config.php
+++ config/config.php
@@ -2,16 +2,16 @@
 
 return [
     // The bucket where the application archive is stored
-    $awsS3Bucket = '',
+    'awsS3Bucket' => '',
 
-    $awsRegion = '',
+    'awsRegion' => '',
 
-    $awsVersion = 'latest',
+    'awsVersion' => 'latest',
 
     // The application name as defined in Codedeploy configuration, usually follows the name of the repository
-    $applicationName = '',
+    'applicationName' => '',
 
     // Deployment groups are named after the ec2 tags used to identify the different machines target of the deploys
-    $deploymentGroups = [
+    'deploymentGroups' => [
     ],
 ];
