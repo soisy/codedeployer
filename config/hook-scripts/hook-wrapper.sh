e33883405f5f7eea8fec4a67a38d1b347120f230
Matteo Riva <matteo.riva@soisy.it>
2019-09-23 14:32:28 +0200

    Add new script templates, improve wrapper

--- config/hook-scripts/hook-wrapper.sh
+++ config/hook-scripts/hook-wrapper.sh
@@ -8,14 +8,16 @@ set -o pipefail
 
 AGENT_ROOT=/opt/codedeploy-agent
 
-COMMAND="${AGENT_ROOT}/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/deploy/hook-scripts/${DEPLOYMENT_GROUP_NAME}/${LIFECYCLE_EVENT}.sh"
-
+SCRIPTS_DIR="${AGENT_ROOT}/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/deploy/hook-scripts"
+COMMAND="${SCRIPTS_DIR}/${DEPLOYMENT_GROUP_NAME}/${LIFECYCLE_EVENT}.sh"
 LOG_LABEL="[${APPLICATION_NAME}:${DEPLOYMENT_GROUP_NAME}:${LIFECYCLE_EVENT}]"
+
+cd "${SCRIPTS_DIR}"
 
 if [[ -f $COMMAND ]]; then
     {
       echo "Execution started" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
-      source "$COMMAND" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
+      source "${COMMAND}" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
       EXIT_CODE=$?
       echo "Execution ended" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
     } >> "${HOME}/logs/deploy.log" 2>&1
